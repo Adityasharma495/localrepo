36fcd96c9c7b49b7ff8aae5370ba73600ef3b22c
unknown <sakshamverma798@gmail.com>
2025-05-05 18:56:29 +0530

    company credits changes - in process

--- src/routes/v2/credit-routes.js
+++ src/routes/v2/credit-routes.js
@@ -9,7 +9,18 @@ router.get("/", AuthMiddleware.validateUser, CreditController.getAll);
 // get credit update by id api/v2/credits/:id
 router.get("/:id", AuthMiddleware.validateUser, CreditController.get);
 
+// get company credit by id api/v2/credits/company/:id
+router.get("/company/:id", AuthMiddleware.validateUser, CreditController.get);
+
 // Update credit update api/v2/credits/:id
+router.post(
+  "/:id",
+  AuthMiddleware.validateUser,
+  CreditMiddleware.validateUpdateRequest,
+  CreditController.updateCredit
+);
+
+// Update credit for company update api/v2/credits/company/:id
 router.post(
   "/:id",
   AuthMiddleware.validateUser,
