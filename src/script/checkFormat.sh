989994e60e36c8a83514965b1e7b997b756a8999
root <vikram@noblestack.com>
2025-05-30 23:45:21 +0530

    changes in prompt duration calculation script

--- src/script/checkFormat.sh
+++ src/script/checkFormat.sh
@@ -4,23 +4,23 @@ path="/home/dev/cloud-telephony/backend/assets/voice/"
 temppath="/home/dev/cloud-telephony/backend/temp/voice/"
 
 # Create prompts directory if it doesn't exist
-if [ ! -d "${path}${1}/prompts/" ]; then
-    mkdir -p "${path}${1}/prompts/"
-    chown -R www-data:www-data "${path}${1}/prompts/"
+if [ ! -d "${path}${1}/prompts/${4}" ]; then
+    mkdir -p "${path}${1}/prompts/${4}"
+    chown -R www-data:www-data "${path}${1}/prompts/${4}"
 fi
 
 # Check if the temp file exists
-if [ -f "${temppath}${1}/prompts/${2}" ]; then
-    format=$(soxi -t "${temppath}${1}/prompts/${2}")
-    rate=$(soxi -r "${temppath}${1}/prompts/${2}")
-    channels=$(soxi -c "${temppath}${1}/prompts/${2}")
-    duration=$(soxi -D "${temppath}${1}/prompts/${2}")
+if [ -f "${temppath}${1}/prompts/${4}/${3}" ]; then
+    format=$(soxi -t "${temppath}${1}/prompts/${4}/${3}")
+    rate=$(soxi -r "${temppath}${1}/prompts/${4}/${3}")
+    channels=$(soxi -c "${temppath}${1}/prompts/${4}/${3}")
+    duration=$(soxi -D "${temppath}${1}/prompts/${4}/${3}")
 
     if [ "$format" = "wav" ]; then
-        sox -t wav "${temppath}${1}/prompts/${2}" -b 16 -c 1 -r 8000 "${path}${1}/prompts/${2}"
+        sox -t wav "${temppath}${1}/prompts/${4}/${3}" -b 16 -c 1 -r 8000 "${path}${1}/prompts/${4}/${3}"
         echo "$duration"
     elif [ "$format" = "mp3" ]; then
-        sox -t mp3 "${temppath}${1}/prompts/${2}" -b 16 -c 1 -r 8000 -t wav "${path}${1}/prompts/${3}"
+        sox -t mp3 "${temppath}${1}/prompts/${4}/${3}" -b 16 -c 1 -r 8000 -t wav "${path}${1}/prompts/${4}/${3}"
         echo "$duration"
     else
         echo "0"
