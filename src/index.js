5ebe4d11ea2b1d65c60c6071bff75e8aced6d55a
Aarzoo-Sandhu <aarzoosandhu2005@gmail.com>
2025-05-30 15:52:03 +0530

    Adding db backup script

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use('/temp', express.static(path.join(__dirname, '../temp')));
 app.use('/api', apiRoutes);
 app.use('/api-docs', swaggerRoutes);
 
-// const { Timezone} = require("../shared/c_db");
+// const { MemberSchedule} = require("../shared/c_db");
 
 const startServer = async () => {
     try {
@@ -34,7 +34,7 @@ const startServer = async () => {
         //     console.log('✅ Database synchronized!');
         // }
 
-        // await Timezone.sync({ alter: true, logging: true });
+        // await MemberSchedule.sync({ alter: true, logging: true });
         
         // console.log('✅ Successfully synced CockroachDB!');
         // Logger.info('CockroachDB -> Successfully synced');
