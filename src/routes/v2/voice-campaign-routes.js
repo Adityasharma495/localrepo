7e4e242c7252a40e11f25f682c57368ddff81ad9
Aarzoo-Sandhu <aarzoosandhu2005@gmail.com>
2025-09-12 15:06:34 +0530

    single call api change

--- src/routes/v2/voice-campaign-routes.js
+++ src/routes/v2/voice-campaign-routes.js
@@ -3,7 +3,7 @@ const { VoiceCampaignMiddleware, AuthMiddleware } = require("../../middlewares")
 const { VoiceCampaignController } = require("../../c_controllers");
 const router = express.Router();
 
-router.post('/singlecall', VoiceCampaignController.HandleSingleCall)
+router.post('/singlecall', AuthMiddleware.validateUser,VoiceCampaignController.HandleSingleCall)
 router.post('/', AuthMiddleware.validateUser, VoiceCampaignMiddleware.ValidateVoiceCampaign,VoiceCampaignController.CreateVoiceCampaign)
 router.get('/', AuthMiddleware.validateUser ,VoiceCampaignController.getCampaigns)
 router.get('/:id', AuthMiddleware.validateUser ,VoiceCampaignController.getOne)
